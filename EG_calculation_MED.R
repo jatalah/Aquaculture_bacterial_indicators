e8bd729823cd11265833705c9bf27d681c761fa5
Javier Atalah <jatalah@gmail.com>
2019-05-13 16:44:54 +0200

    nuevos datos ASV

--- EG_calculation_MED.R
+++ EG_calculation_MED.R
@@ -3,163 +3,258 @@ library(tidyverse)
 library(splines)
 library(quantreg)
 library(knitr)
+library(pracma)# for finding peaks
+library(ggpubr)
+library(vegan)
+
 source('theme_javier.R')
 theme_set(theme_javier())
 
-#read salmon farm foram DNA only data --------------
-dna <- 
-  read_csv('BacteriaMed_all.csv',col_types = cols())
+#read data --------------
+dna <- read_csv('data/Bacteria16SMed_Table.csv',col_types = cols())
 
-# select 150 most abundant forams OTUs------------
-abund_OTU <- 
+# select 500 most abundant ASV------------
+abund_ASV <-  
   dna %>% 
-  gather(OTU, abund, OTU1:OTU12955) %>% 
-  group_by(OTU) %>% 
+  gather(ASV, abund, ASV_001:ncol(.)) %>% 
+  group_by(ASV) %>% 
   summarise_at(vars(abund), funs(sum), na.rm =T) %>%
-  top_n(200) %>% 
-  dplyr::select(OTU) %>% 
+  top_n(500) %>% 
+  dplyr::select(ASV) %>% 
   as.list()
 
 dna_top <- 
   dna %>% 
-  select(Station, Distance, Installation, TFS, abund_OTU$OTU)
+  select(Station, Distance, Installation, TFS, abund_ASV$ASV)
 
-# convert into long format---------------
+# convert into long format
 dna_top_long <- 
   dna_top %>% 
-  gather(OTU,abund, abund_OTU$OTU)
+  gather(ASV,abund, abund_ASV$ASV)
 
 # create plot with quantile regression and splines with 3 df--------------
 p2 <- 
-  ggplot(dna_top_long, aes(x = Distance, y = abund, color = Installation)) +
+  ggplot(dna_top_long, aes(x = TFS, y = abund, color = Installation)) +
   geom_point(size = 3, alpha = .3) +
-  labs(x = 'Distancia (m)', y = 'Number of reads') +
+  labs(x = 'Total free sulphides', y = 'Number of reads') +
   stat_quantile(method = rq,
                 formula = y ~ bs(x, df = 3),
                 quantiles = 0.95) +
-  facet_wrap(~OTU, scales = 'free')
+  facet_wrap(~ASV, scales = 'free')
 
 
 # get splines data from plot------------------------
 gb <- ggplot_build(p2)
 
-# obtaine ES values whre abundance peak by year-------
+# obtaine ES values where abundance peak by installation-------
 gb1 <- 
   gb$data[[2]] %>% 
   data.frame() %>% 
   group_by(group,PANEL) %>% 
   nest() %>% 
-  mutate(peak = map(data, ~.$x[which.max(.$y)]))
-
-peak_dat <- 
-  gb1$peak %>% 
-  flatten_dbl() %>% 
-  data.frame() %>% 
-  rename(Peak = '.') %>% 
-  bind_cols(dna_top_long %>% distinct(OTU,Installation)) %>% 
-  spread(Installation,Peak) %>% 
-  mutate(diff = abs(Farm2 - Farm1)) %>% 
-  filter(diff<100) %>% 
-  gather(Installation, Peak, Farm2:Farm1) %>% 
-  select(-diff)
+  mutate(
+    peak = map(data, ~ .$x[which.max(.$y)]),
+    find_peaks = map(data, ~ findpeaks(
+      .$y,
+      nups = 0,
+      ndowns = 0,
+      minpeakheight = max(.$y) / 2.4
+    )),
+    n_peaks = map_dbl(find_peaks, nrow)
+  )
 
 
-long_top_OTU <- 
-  inner_join(dna_top_long, peak_dat, by = c('OTU', 'Installation'))
+plot_id <- 
+  gb$plot[[1]] %>% 
+  distinct(Installation,ASV) %>% 
+  arrange(ASV)
+
+
+# get the number of peaks per AVS and year ------
+n_peaks <- 
+  gb1 %>% 
+  select(n_peaks) %>% 
+  unnest() %>% 
+  bind_cols(plot_id)
+
+
+# merge difference peaks by Installation  and the number of peaks-----
+peak_dat <-
+  gb1 %>%
+  select(peak) %>%
+  unnest() %>%
+  bind_cols(plot_id) %>%
+  spread(Installation, peak) %>%
+  mutate(diff = abs(Ext - Int)) %>%
+  gather(Installation, Peak, Ext:Int) %>%
+  full_join(., n_peaks, by = c('ASV', 'Installation')) %>% # join number of peaks data
+  mutate(# quality of assigments
+    Quality = as.numeric(as.character(cut(
+      diff,
+      breaks = c(0, 100, 200, 1000), # difference where ASV reads peak in relation to TFS 
+      labels = c(1, 2, 3)
+    ))),
+    Quality = if_else(n_peaks > 2, 4, Quality)) # double peaks)
+
+
+# # need to filter OTU that have Quality<3 on both years----
+top_qual_ASV <- 
+  peak_dat %>%
+  select(ASV, Installation, Quality) %>%
+  spread(Installation, Quality) %>%
+  drop_na() %>% 
+  filter(Ext<3 & Int <3) %>% 
+  select(ASV) %>% 
+  as.list()
+
+## join peak_dat with splines and ASV reads abundance data --------------
+long_top_ASV <- 
+  full_join(dna_top_long, peak_dat, by = c('ASV', 'Installation')) %>% 
+  dplyr::filter(ASV %in% top_qual_ASV$ASV)
   
-## plot selected OTUs with peak vertical lines--------------
-plot_distance <-
-  ggplot(long_top_OTU, aes(x = Distance, y = abund, color = Installation)) +
+## plot selected ASVs with peak vertical lines--------------
+plot_TFS <-
+  ggplot(long_top_ASV, aes(x = TFS, y = abund, color = Installation)) +
   geom_point(size = 2, alpha = .3) +
-  labs(x = 'Distance to the farms (m)', y = 'Number of reads') +
+  labs(x = 'Total free sulfides', y = 'Number of reads') +
   theme_javier() +
   stat_quantile(method = rq,
                 formula = y ~ bs(x, df = 3),
                 quantiles = 0.95) +
-  facet_wrap(~ OTU, scales = 'free') +
-  geom_vline(aes(xintercept = Peak, color = Installation))
+  geom_vline(aes(xintercept = Peak, color = Installation)) +
+  facet_wrap(~ ASV, scales = 'free') 
 
-ggsave(plot_distance, 
-       filename = 'Distance_MED.tiff',
-       width = 15,
-       height = 10,
+ggsave(plot_TFS, 
+       filename = 'figures/TFS_splines.tiff',
+       width = 20,
+       height = 20,
        device = 'tiff',
        compression = 'lzw')
-
-## Sulphide gradient ---------------
-# create plot with quantile regression and splines with 3 df--------------
-p3 <- 
-  ggplot(dna_top_long, aes(x = TFS, y = abund, color = Installation)) +
-  geom_point(size = 3, alpha = .3) +
-  labs(x = 'Distancia (m)', y = 'Number of reads') +
-  theme_bw() +
-  stat_quantile(method = rq,
-                formula = y ~ bs(x, df = 3),
-                quantiles = 0.95) +
-  facet_wrap(~OTU, scales = 'free')
-
-
-# get splines data from plot------------------------
-gb_tfs <- ggplot_build(p3)
-
-# obtaine ES values whre abundance peak by year-------
-gb_tfs1 <- 
-  gb_tfs$data[[2]] %>% 
-  data.frame() %>% 
-  group_by(group,PANEL) %>% 
-  nest() %>% 
-  mutate(peak = map(data, ~.$x[which.max(.$y)]))
-
-peak_dat_tfs <- 
-  gb_tfs1$peak %>% 
-  flatten_dbl() %>% 
-  data.frame() %>% 
-  rename(Peak = '.') %>% 
-  bind_cols(dna_top_long %>% distinct(OTU,Installation)) %>% 
-  spread(Installation,Peak) %>% 
-  mutate(diff = abs(Farm2 - Farm1)) %>% 
-  filter(diff<100) %>% 
-  gather(Installation, Peak, Farm2:Farm1) %>% 
-  select(-diff)
-
-
-long_top_OTU_tfs <- 
-  inner_join(dna_top_long, peak_dat_tfs, by = c('OTU', 'Installation'))
-
-## plot selected OTUs with peak vertical lines--------------
-plot_tfs <- 
-  ggplot(long_top_OTU_tfs, aes(x = TFS, y = abund, color = Installation)) +
-  geom_point(size = 2, alpha = .3) +
-  labs(x = 'Total free sulfides', y = 'Number of reads') +
-  stat_quantile(method = rq,
-                formula = y ~ bs(x, df = 3),
-                quantiles = 0.95) +
-  facet_wrap( ~ OTU, scales = 'free') +
-  geom_vline(aes(xintercept = Peak, color = Installation))
-
-ggsave(plot_tfs, 
-       filename = 'TFS_MED.tiff',
-       width = 15,
-       height = 10,
-       device = 'tiff',
-       compression = 'lzw')
-
-print(plot_tfs)
 
 # EG assigment ----------
-EG_groups_MED <-
-  peak_dat_tfs %>%
+EG_groups <- 
+  peak_dat %>%
+  dplyr::filter(ASV %in% top_qual_ASV$ASV) %>%  
+  group_by(ASV) %>% 
+  summarise(mean_peak = mean(Peak)) %>% 
   mutate(EG = cut(
-    Peak,
-    breaks= c(0,quantile(dna$TFS,probs = c( 1/4,2/4,3/4)),max(dna$TFS)),
-    labels = c("I", "II", "III","IV")
+    mean_peak,
+    breaks= c(0, quantile(dna$TFS,probs = c( 1/5,2/5,3/5, 4/5)),max(dna$TFS)),
+    labels = c("I", "II", "III","IV", "V")
   )) %>% 
-  select(OTU,EG) %>% 
-  write_csv(.,'EG_groups_MED.csv')
+  dplyr::select(ASV,EG) %>% 
+  write_csv('outputs/EG_groups_MED.csv')
 
 # print the table with EG groups-------------
 
-kable(breaks)
+kable(EG_groups, 
+      caption = "EcoGroup assigment for the 100 most abundant foraminifera ASVs based on quantile spline regressions against ES")  
 
-kable(EG_groups_MED, 
-      caption = "EcoGroup assigment for the 100 most abundant foraminifera OTUs based on quantile spline regressions against ES")  
+
+pp1 <- 
+  ggplot(EG_groups, aes(fct_rev(EG))) + 
+  geom_bar(aes(y = (..count..)/sum(..count..)), color = 1) + 
+  scale_y_continuous(labels=scales::percent) +
+  labs(y  = "Relative frequencies", x = "Eco_Group") +
+  coord_flip()
+
+pp2 <- 
+  ggplot(peak_dat, aes(fct_rev(factor(Quality)))) + 
+  geom_bar(aes(y = (..count..)/sum(..count..)), color = 1) + 
+  scale_y_continuous(labels=scales::percent) +
+  labs(y  = "Relative frequencies", x = "Spline Quality Score") +
+  coord_flip()
+
+
+ggsave(ggarrange(pp1,pp2), 
+       filename = 'figures/EG_assigment_summary.tiff',
+       device = 'tiff',
+       compression = 'lzw',
+       width = 8,
+       height = 4,
+       dpi = 300)
+
+
+# Calculate the bacterial AMBI------------------
+
+# read AVS read abundance and eco-groups data-----------------------------------
+env <-  
+  read_csv('data/Bacteria16SMed_Table.csv',col_types = cols()) %>%
+  select(Station:TOC)
+
+data <- 
+  read_csv('data/Bacteria16SMed_Table.csv',col_types = cols()) %>%
+  select(Station, ASV_001:ncol(.))
+
+eg <- read_csv('outputs/EG_groups_MED.csv')
+
+# EG proportion calculations --------------
+EG_indices <-
+  data %>%
+  gather(ASV, abund, -Station) %>%
+  left_join(eg, by = 'ASV') %>%
+  group_by(Station) %>%
+  mutate(
+    N = sum(abund),
+    H = diversity(abund),
+    S = specnumber(abund),
+    J = H / log(S),
+    UA = sum(is.na(EG) & abund > 0) / S * 100
+  ) %>%
+  drop_na(EG) %>%
+  mutate(N1 = sum(abund),
+         S1 = specnumber(abund)) %>%
+  group_by(Station, EG) %>%
+  mutate(prop_n = sum(abund) / N1,
+         prop_s = specnumber(abund) / S1) %>%
+  summarise(
+    n = mean(prop_n, na.rm = T) * 100,
+    s = mean(prop_s, na.rm = T) * 100,
+    H = mean(H),
+    S = mean(S),
+    J = mean(J),
+    N = mean(N),
+    N1 = mean(N1),
+    S1 = mean(S1),
+    UA = mean(UA)
+  ) %>%
+  gather(key, value, n, s) %>%
+  unite(EG_comb, EG, key) %>%
+  spread(EG_comb, value) 
+
+
+# Combine all indices with metadata----------------
+EG_prop_all <- left_join(EG_indices, env, by = 'Station')
+
+# Calculate optimal weights for AMBI using linear regression on the train dataset-------
+AMBI_weights <- lm(TFS ~  0 + I_n + II_n + III_n + IV_n + V_n, data = EG_prop_all)
+
+indices_all <-
+  EG_prop_all %>%
+  mutate(
+    AMBI1 = (
+      1.5 * II_n + 
+        3 * III_n + 
+        4.5 * IV_n + 
+        6 * V_n
+    ),
+    AMBI2 = (
+      AMBI_weights$coefficients[1] * I_n +
+        AMBI_weights$coefficients[2] * II_n + 
+        AMBI_weights$coefficients[3] * III_n + 
+        AMBI_weights$coefficients[4] * IV_n + 
+        AMBI_weights$coefficients[5] * V_n
+    )/100
+  )
+
+# Plot euk-AMBI vs macrofaunal AMBI-----------
+ggplot(indices_all, aes(TFS, AMBI2)) +
+  geom_point(size = 4, aes(color  = Distance)) +
+  geom_smooth(method = "lm") +
+  stat_poly_eq(
+    formula = y ~ x,
+    aes(label = ..rr.label..),
+    parse = TRUE,
+    size = 4
+  ) +
+  labs(y = 'Bacterial-AMBI', x =  'Total free sulphide (TFS)')
